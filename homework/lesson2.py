ee53bf4daf3ce69efbcf85b4b9f473b75cbdfa47
winnerchou <33021992+winnerchou@users.noreply.github.com>
2018-07-09 19:11:35 +0800

    Initial commit

---
+++ homework/lesson2.py
@@ -0,0 +1,106 @@
+#homework lesson 2 第一題
+#x=input("請輸入數字: ") #取得字串形式的使用者輸入
+#x=int(x) #將字串轉換成數字型態
+# if x>100:
+ #   print("大於100")
+#else:
+ #   print("小於等於100")
+#x=input("請輸入數字: ")
+#x=int(x)
+#if x>200:
+#    print("大於200")
+#elif x>100:
+#    print("大於100 小於等於200")
+#else:
+#    print("小於等於100")
+# 四則運算
+# 
+# print("第二次課程作業一：四則運算式")
+# n1=int(input("請輸入數字一："))
+# n2=int(input("請輸入數字二："))
+# op=input("請輸入運算：+,-,*,/：")
+# if op=="+":
+#     print(n1+n2)
+# elif op=="-":
+#     print(n1-n2)
+# elif op=="*":
+#     print(n1*n2)
+# elif op=="/":
+#     print(n1/n2)
+# else:
+#    print("不支援的運算")
+
+
+# n1=int(input("輸入數字一: "))
+# n2=int(input("輸入數字二: "))
+#print(n1+n2)
+#n1=int(input("請輸入數字一: "))
+#n2=int(input("請輸入數字二: "))
+#op=input("請輸入運算: +,-,*,/:")
+#if op=="+":
+#    print(n1+n2)
+#elif op=="-":
+#    print(n1-n2)
+#elif op=="*":
+#    print(n1*n2)
+#elif op=="/":
+#    print(n1/n2)
+#else:
+#    print("不支援的運算")
+#x=int(input("請輸入數字:"))
+#x=int(x**0.5)
+#print(x)
+#while 迴圈
+#for  迴圈
+#while 迴圈
+#n=1
+#while n<=3:
+#    print(n)
+#   n+=1
+# 計算累加的結果:1+2+3+...+10
+#n=1
+#sum=0  #紀錄累加的結果
+#while n<=10:
+#    sum=sum+n
+#   n+=1
+#print(sum)
+#注意:sum=sum+n與n+=1順序不可顛倒，否則答案會錯
+#for 迴圈
+#for x in [3,5,1]:
+#    print(x)
+#for x in "Hello":
+#    print(x)
+#for x in range(5):
+#    print(x)
+#for x in range(5,10):
+#   print(x)
+#用for 迴圈來運算1+2,...+10
+# sum=0
+# for x in range(1,11):
+#     sum=sum+x
+# print(sum)
+#重要:比較兩種方式的邏輯
+
+#print("第二次課程作業三：九九乘法表")
+# for x in range(1,10):
+#     for y in range(1,10):
+#         print(x,"*",y," =",x*y)
+
+#print("第二次課程作業二：整數平方根")
+#x=int(input("請輸入一個正整數："))
+# for n in range(int(x)):
+#     n+=1
+#     if x==n*n:
+#         print("此數值的平方根=  ",n)
+#         break
+# else:
+#         print("此數值沒有整數平方根")
+
+# y = int(x**(0.5))
+# if y*y != x:
+#     print('此數值沒有整數平方根')
+# else:
+#     print(y)
+
+
+
---
+++ start.py
@@ -0,0 +1,37 @@
+# print("good try")
+# print("you will getting better")
+# print("中文測試")
+# # 這是註解，寫給人看的
+# 3.5
+# # print(資料)
+# #字串
+# "任意的文字"
+# print("任意的文字")
+# # 布林值
+# True
+# False
+# print(True)
+# # 變數. 把資料 3 放進去
+# input=3
+# print(input)
+# input="Hello World"
+# print(input)
+# 運算符號
+data=5//2 # 整數除法
+print(data)
+data=6*2
+print(data)
+data=6**3 # 6的3次方
+print(data)
+print("Hello"+"World")
+# input(提示文字) 以[字串的的型態]取得使用者輸入
+3+4
+"3"+"4"
+n!=input("Enter a Number:")
+n2=input("Enter a Number:")
+n1=int(n1) #int(資料) 將資料轉換成整數型態
+n2+int(n2)
+result=n1+n2
+print(result)
+# 作業=讓使用者輸入5個數字。1.算總和 2.哪個數字最大
+# 使用 List 列表處理
